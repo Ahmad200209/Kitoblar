33169e6c05c7dfcda588181ffa1c470e364a37cb
Ahmad <129722187+Ahmad200209@users.noreply.github.com>
2024-03-30 00:27:31 +0500

    f

--- ummumiy.py
+++ ummumiy.py
@@ -64,8 +64,6 @@ async def salok(context: ContextTypes.DEFAULT_TYPE) -> None:
         kun = hozir.day
         oy = oylar[hozir.strftime('%B')]
         yil = hozir.year
-        global counter
-        counter += 1
         gregorian_date = hozir
         hijri_date = convert.Gregorian(gregorian_date.year, gregorian_date.month, gregorian_date.day).to_hijri()
         salomlash = f"👋Assalomu alaykum va rahmatullohi va barakatuh \nKitob do`konimizning qadrli a`zolari \n📚📚📚Allox sizning bugungi kuningizni mutoalaga boy qilsin ..!!! \nOmiyn.\nBugun haftaning {hafta} kuni {kun}- {oy} {yil}-yil\nHijriy: {hijri_date.day} - {hijri_months[hijri_date.month - 1]} {hijri_date.year}-yil\n👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻\nBizning rasmiy manzilimiz: https://t.me/Onxayat_kitoblar"
